2d4917b1ecb41740e3ef214f3e5dce2620fb8b99
Marek Kramarz <markoni@spoko.pl>
2025-05-12 12:22:58 +0200

    feat: show nodes table after calculating cpm

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,62 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import EventsList from './components/EventsList.vue';
+import NodesTable from './components/NodesTable.vue';
+
+import { store } from './store';
+
+type ViewTarget = 'Table' | 'Events';
+
+const showNodesTable = ref<boolean>(false);
+
+function switchViews(target: ViewTarget) {
+  showNodesTable.value = target === 'Table';
+}
 </script>
 
 <template>
-  <div id="app-lower-xd">
-    <EventsList />
+  <div id="app-lower">
+    <div
+      v-if="store.nodes.value.length > 0 && !showNodesTable"
+      class="arrow-right"
+      @click="() => switchViews('Table')"
+    >
+      ➡️
+    </div>
+    <div
+      v-if="showNodesTable"
+      class="arrow-left"
+      @click="() => switchViews('Events')"
+    >
+      ⬅️
+    </div>
+
+    <EventsList v-if="!showNodesTable" />
+    <NodesTable v-if="showNodesTable" />
   </div>
 </template>
 
 <style scoped>
-#app-lower-xd {
+#app-lower {
   width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.arrow-left,
+.arrow-right {
+  position: fixed;
+  top: 10px;
+  font-size: 24px;
+  cursor: pointer;
+}
+
+.arrow-left {
+  left: 10px;
+}
+.arrow-right {
+  right: 10px;
 }
 </style>
